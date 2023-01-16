098d0cf7d3da88636da02627d201ecd61f6ec678
Methian <mstankaitis9@gmail.com>
2023-01-17 00:33:33 +0200

    first commit

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('Hello', [AuthController::class]);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
